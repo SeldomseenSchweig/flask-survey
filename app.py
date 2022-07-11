54655041476fd3d6f7a55d8fd4911c383b24e51d
SeldomseenSchweig <jamesschweig@gmail.com>
2022-07-11 07:52:10 -0700

    I added cookies to keep track of the user completing the survey

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
+from crypt import methods
 from dis import Instruction
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import Survey, Question, satisfaction_survey
 
 
 responses = []
-page = 0
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "hayduke"
 
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS']= False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS']= True
 
 debug = DebugToolbarExtension(app)
+
+@app.route('/set_session', methods=["POST"])
+def set_session():
+    """ Setting session cookies to an empty list and redirecting to """
+    session['responses'] = []
+    return redirect('/question/0')
 
 
 @app.route('/')
 def introduce_survey():
+    if  len(satisfaction_survey.questions) == len(session['responses']):
+        flash("You have already completed the survey!", 'error')
+
+        return redirect('/end_of_survey')
     """" This will be the first page and introduce the survey """
     title = satisfaction_survey.title 
     instructions = satisfaction_survey.instructions
@@ -33,26 +44,15 @@ def question(number):
     put in a question they shouldn't access 
     it will send them to the Q that they are on"""
 
-    
-
-    # if not number == len(responses):
-    #     flash("You tried to access a question out of order", 'error')
-    #     new_page = ""
-    #     new_page =  "/question/" + str(len(responses))
-    #     return redirect(new_page)
-    # elif len(satisfaction_survey.questions) == len(responses):
-    #     flash("You have already completed the survey!", 'error')   
-    #     return redirect('/end_of_survey')
-
-    if  len(satisfaction_survey.questions) == len(responses):
+    if  len(satisfaction_survey.questions) == len(session['responses']):
         flash("You have already completed the survey!", 'error')
-        new_page = ""
-        new_page =  "/question/" + str(len(responses))
+
         return redirect('/end_of_survey')
-    elif not number == len(responses):
+    elif not number == len(session['responses']):
+        new_page = ""
+        new_page =  "/question/" + str(len(session['responses']))
         flash("You tried to access a question out of order", 'error')    
         return redirect(new_page)
-
 
     question = satisfaction_survey.questions[number].question
     options = satisfaction_survey.questions[number].choices
@@ -61,19 +61,25 @@ def question(number):
 @app.route('/answer', methods=["POST"])
 def add_response():
     new_page = ""
+
     """This will send the answer to the response list and 
     redirect to next question or to the end of the survey Thank You page, with a flash"""
     answer = request.form['response']
+    # responses = session['responses']
+   
     responses.append(answer)
-    new_page =  "/question/" + str(len(responses))
+    session['responses'] = responses
+    new_page =  "/question/" + str(len(session['responses']))
     
-    if len(satisfaction_survey.questions) == len(responses):      
+    if len(satisfaction_survey.questions) == len(session['responses']):      
         return redirect('/end_of_survey')
     else:
         return redirect(new_page)
 
 @app.route('/end_of_survey')
 def end_of_survey():
-        return render_template('thank_you.html')
+    print("*******************************")
+    print(session['responses'])
+    return render_template('thank_you.html')
 
 
